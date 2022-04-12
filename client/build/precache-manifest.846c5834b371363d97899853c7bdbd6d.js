1377235677ca4021b64dcbb5f1fc147396818fdc
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-04-11 21:17:53 -0500

    create build folder

---
+++ client/build/precache-manifest.846c5834b371363d97899853c7bdbd6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f002b36110f39d91ab54201af22649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bee39e62b5b2f0118294",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "c36bc4005f96902a0f7b",
+    "url": "/static/js/2.f3c2e3e9.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.f3c2e3e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee39e62b5b2f0118294",
+    "url": "/static/js/main.5c9db137.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
