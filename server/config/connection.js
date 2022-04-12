2bf7c31d26e2d026837a2e6ff110ccb2441ddb47
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-04-11 21:46:55 -0500

    add catch event for connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(() => {
+  console.log("Connection successfull");
+}).catch((e) => console.log("No connection"));
+
 
 module.exports = mongoose.connection;
